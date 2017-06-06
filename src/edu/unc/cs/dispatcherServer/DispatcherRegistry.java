93c65005726b147399fb2ce5cb29c88e285ff2b6
pdewan <dewan@cs.unc.edu>
2017-06-06 02:28:42 -0400

    Added a comment to registry to connect projects

--- src/edu/unc/cs/dispatcherServer/DispatcherRegistry.java
+++ src/edu/unc/cs/dispatcherServer/DispatcherRegistry.java
@@ -3,7 +3,10 @@ package edu.unc.cs.dispatcherServer;
 import gradingTools.server.RemoteGraderServer;
 
 import java.rmi.Remote;
-
+/**
+ * This is the interface to be linked by users of the server.
+ * The class is in project GraderDispatcherServer
+ */
 public interface DispatcherRegistry extends Remote{
 
 	void registerDriverServer (RemoteGraderServer aServerObject, GraderServerDescription aDispatcherDescription);
