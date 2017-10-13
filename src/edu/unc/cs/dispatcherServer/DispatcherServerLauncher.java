7081e52c52bf09c7c91de4678b26220e3eab021c
avitkus <avitkus7@gmail.com>
2017-10-13 02:15:33 -0400

    Changes to work with restarting in server

--- src/edu/unc/cs/dispatcherServer/DispatcherServerLauncher.java
+++ src/edu/unc/cs/dispatcherServer/DispatcherServerLauncher.java
@@ -1,12 +1,13 @@
 package edu.unc.cs.dispatcherServer;
 
+import java.util.concurrent.Future;
 
 public interface DispatcherServerLauncher {
 	public static String DISPATCHER_SERVER_NAME = "dispatcher server";
 	public static final String DISPATCHER_SERVER_ID = "31452";
 	public static final Class DISPATCHER_SERVER_CLASS = DispatcherRegistry.class;
 	String getCommand(String aClientName);
-	Process exec(String aClientName);
+	Future<Void> exec(String aClientName);
 	
 
 
